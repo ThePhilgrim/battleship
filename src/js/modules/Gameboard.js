82377dd4f6757f2ab10ebdae1b98034c701bb369
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-10-29 08:49:50 +0100

    Add logic for placing ship in valid spot

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -6,6 +6,23 @@ export default class GameBoard {
   receiveHit(x, y) {
     this.grid[x][y].hasReceivedHit = true;
   }
+
+  placeShip(ship, x, y) {
+    // if (!placementIsValid(ship, startSquare)) {
+    //   return;
+    // }
+
+    if (ship.isVertical) {
+      for (let i = x; i < x + ship.length; i += 1) {
+        this.grid[i][y].containsShip = ship;
+      }
+    } else {
+      const gridRow = this.grid[x];
+      for (let i = y; i < y + ship.length; i += 1) {
+        gridRow[i].containsShip = ship;
+      }
+    }
+  }
 }
 
 class boardSquare {
