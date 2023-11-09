61f6ad6e536968b8f206d252f12baacdb168de5a
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-11-09 23:40:50 +0100

    Add test for AI placing ships

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -5,25 +5,33 @@ let humanPlayer;
 let aiPlayer;
 
 describe('Testing the AI player', () => {
-  describe('Tests where the returned attack coordinates are fixed', () => {
-    let aiPlayer;
+  beforeAll(() => {
+    aiPlayer = new AI();
+  });
 
-    beforeAll(() => {
-      aiPlayer = new AI();
-      jest
-        .spyOn(aiPlayer, 'getRandomCoordinates')
-        .mockReturnValueOnce({ x: 3, y: 6 })
-        .mockReturnValueOnce({ x: 3, y: 6 })
-        .mockReturnValueOnce({ x: 1, y: 8 });
-    });
+  test('Make sure duplicate coordinates are not added to this.attackedSquares', () => {
+    jest
+      .spyOn(aiPlayer, 'getRandomCoordinates')
+      .mockReturnValueOnce({ x: 3, y: 6 })
+      .mockReturnValueOnce({ x: 3, y: 6 })
+      .mockReturnValueOnce({ x: 1, y: 8 });
 
-    test('Make sure duplicate coordinates are not added to this.attackedSquares', () => {
-      expect(aiPlayer.attackedSquares).toHaveLength(0);
-      aiPlayer.attack();
-      expect(aiPlayer.attackedSquares).toHaveLength(1);
-      aiPlayer.attack();
-      expect(aiPlayer.attackedSquares).toHaveLength(2);
-      expect(aiPlayer.getRandomCoordinates).toHaveBeenCalledTimes(3);
-    });
+    expect(aiPlayer.attackedSquares).toHaveLength(0);
+    aiPlayer.attack();
+    expect(aiPlayer.attackedSquares).toHaveLength(1);
+    aiPlayer.attack();
+    expect(aiPlayer.attackedSquares).toHaveLength(2);
+    expect(aiPlayer.getRandomCoordinates).toHaveBeenCalledTimes(3);
+  });
+
+  test('Place ships', () => {
+    const shipsToPlace = [...aiPlayer.ships];
+
+    while (shipsToPlace.length > 0) {
+      let ship = shipsToPlace.shift();
+      aiPlayer.placeShip(ship);
+    }
+
+    expect(aiPlayer.gameboard.ships.length).toBe(5);
   });
 });
