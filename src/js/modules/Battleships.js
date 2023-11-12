314b19b6b601a80c41fe6c069e9d70349f133253
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-11-12 11:23:55 +0100

    Make game loop update UI squares

--- src/js/modules/Battleships.js
+++ src/js/modules/Battleships.js
@@ -1,5 +1,4 @@
 import { Human, AI } from './Player';
-import Ship from './Ship';
 import GameBoard from './Gameboard';
 import UserInterface from './UI';
 
@@ -9,7 +8,6 @@ export default class Battleships {
     player2 = new AI(new GameBoard()),
     userInterface = new UserInterface()
   ) {
-    console.log('bs');
     this.player1 = player1;
     this.player2 = player2;
     this.userInterface = userInterface;
@@ -25,16 +23,51 @@ export default class Battleships {
     console.groupEnd();
   }
 
+  updateSquares(attackingPlayerTables, receivingPlayerTables, coordinatesToUpdate, classNames) {
+    coordinatesToUpdate.forEach((coordinates) => {
+      this.userInterface.updateSquare(attackingPlayerTables.opponentTable, coordinates, classNames);
+
+      this.userInterface.updateSquare(receivingPlayerTables.playerTable, coordinates, classNames);
+    });
+  }
+
+  getCoordinatesArray(attackResult, receivingPlayer, coordinates) {
+    // Coordinates is an array when a ship is sunk
+    if (attackResult === 'SUNK') {
+      const { y, x } = coordinates;
+      return receivingPlayer.gameboard.grid[y][x].containsShip.ownCoordinates;
+    } else {
+      return [coordinates];
+    }
+  }
+
   async startNewGame() {
     const playersInitialized = [this.player1.placeShips(), this.player2.placeShips()];
 
     await Promise.all(playersInitialized);
     let roundNo = 1;
+
+    const resultClassNames = {
+      MISS: ['attacked'],
+      HIT: ['attacked'],
+      SUNK: ['attacked', 'sunk'],
+    };
+
     while (!(this.player1.gameboard.allShipsSunk() || this.player2.gameboard.allShipsSunk())) {
       console.log('Round No: ', roundNo);
       const player1Coordinates = await this.player1.getCoordinates(this.player1.gameboard);
       this.player1.rememberAttack(player1Coordinates);
-      this.player2.receiveAttack(player1Coordinates);
+      const attackResult1 = this.player2.receiveAttack(player1Coordinates);
+      console.log('ATTACK RESULTS: ', attackResult1);
+
+      const coordinatesToUpdate1 = this.getCoordinatesArray(attackResult1, this.player2, player1Coordinates);
+
+      this.updateSquares(
+        this.userInterface.player1Tables,
+        this.userInterface.player2Tables,
+        coordinatesToUpdate1,
+        resultClassNames[attackResult1]
+      );
 
       if (this.player2.gameboard.allShipsSunk()) {
         console.log('PLAYER 1 WINS');
@@ -44,9 +77,16 @@ export default class Battleships {
       console.log('player2');
       const player2Coordinates = await this.player2.getCoordinates(this.player2.gameboard);
       this.player2.rememberAttack(player2Coordinates);
-      this.player1.receiveAttack(player2Coordinates);
+      const attackResult2 = this.player1.receiveAttack(player2Coordinates);
 
-      this.printGameState(roundNo);
+      const coordinatesToUpdate2 = this.getCoordinatesArray(attackResult2, this.player1, player2Coordinates);
+
+      this.updateSquares(
+        this.userInterface.player2Tables,
+        this.userInterface.player1Tables,
+        coordinatesToUpdate2,
+        resultClassNames[attackResult2]
+      );
 
       if (this.player2.gameboard.allShipsSunk()) {
         console.log('PLAYER 2 WINS');
@@ -54,8 +94,10 @@ export default class Battleships {
       }
 
       roundNo += 1;
-      // await delay();
+      await delay();
     }
+
+    // Print winner
   }
 }
 
@@ -64,11 +106,3 @@ function delay() {
     setTimeout(done, 50);
   });
 }
-
-// GameMaster needs
-// - Player 1 (Always human)
-// - Player 2 (Default AI)
-
-// Player needs
-// - Gameboard
-// - List of Ships
