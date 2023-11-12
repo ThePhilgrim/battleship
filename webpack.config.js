5237f6b6acc55dfead0215a46b9b6b99da2fe0d3
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-11-13 00:41:18 +0100

    Fix webpack/jest not working with ESM imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+// https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
