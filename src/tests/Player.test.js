771345042650c9b5e9687b0c9d96f11a6617891d
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-11-13 18:57:45 +0100

    Add new tests for AI player

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,65 +1,144 @@
-import { AI, Human } from '../js/modules/Player';
-import { jest } from '@jest/globals';
+import { AI, Human } from '../js/modules/Player.js';
+import Gameboard from '../js/modules/Gameboard.js';
+// import { jest } from '@jest/globals';
 
-let humanPlayer;
 let aiPlayer;
 
-// describe('Testing the AI player', () => {
-//   beforeAll(() => {
-//     aiPlayer = new AI();
-//   });
+describe('Testing the AI player', () => {
+  beforeEach(() => {
+    aiPlayer = new AI(new Gameboard());
+  });
 
-//   test('Make sure duplicate coordinates are not added to this.attackedSquares', () => {
-//     jest
-//       .spyOn(aiPlayer, 'getRandomCoordinates')
-//       .mockReturnValueOnce({ x: 3, y: 6 })
-//       .mockReturnValueOnce({ x: 3, y: 6 })
-//       .mockReturnValueOnce({ x: 1, y: 8 });
+  test('Test placeShips() to ensure all ships are placed.', () => {
+    expect(aiPlayer.gameboard.ships.length).toBe(0);
+    aiPlayer.placeShips();
+    expect(aiPlayer.gameboard.ships.length).toBe(aiPlayer.ships.length);
+  });
 
-//     expect(aiPlayer.attackedSquares).toHaveLength(0);
-//     aiPlayer.attack();
-//     expect(aiPlayer.attackedSquares).toHaveLength(1);
-//     aiPlayer.attack();
-//     expect(aiPlayer.attackedSquares).toHaveLength(2);
-//     expect(aiPlayer.getRandomCoordinates).toHaveBeenCalledTimes(3);
-//   });
+  test('Ensure getCoordinates() base case', () => {
+    const coordinates = aiPlayer.getCoordinates(new Gameboard());
+    expect(coordinates).toHaveProperty('x');
+    expect(coordinates).toHaveProperty('y');
+  });
 
-//   test('Place ships', () => {
-//     const shipsToPlace = [...aiPlayer.ships];
-//     let shipsPlaced = 0;
+  test('Ensure getCoordinates() does not return coordinates already in Player.attackedSquares', () => {
+    // Missing { y: 9, x: 9 }
+    aiPlayer.attackedSquares = [
+      { y: 0, x: 0 },
+      { y: 0, x: 1 },
+      { y: 0, x: 2 },
+      { y: 0, x: 3 },
+      { y: 0, x: 4 },
+      { y: 0, x: 5 },
+      { y: 0, x: 6 },
+      { y: 0, x: 7 },
+      { y: 0, x: 8 },
+      { y: 0, x: 9 },
+      { y: 1, x: 0 },
+      { y: 1, x: 1 },
+      { y: 1, x: 2 },
+      { y: 1, x: 3 },
+      { y: 1, x: 4 },
+      { y: 1, x: 5 },
+      { y: 1, x: 6 },
+      { y: 1, x: 7 },
+      { y: 1, x: 8 },
+      { y: 1, x: 9 },
+      { y: 2, x: 0 },
+      { y: 2, x: 1 },
+      { y: 2, x: 2 },
+      { y: 2, x: 3 },
+      { y: 2, x: 4 },
+      { y: 2, x: 5 },
+      { y: 2, x: 6 },
+      { y: 2, x: 7 },
+      { y: 2, x: 8 },
+      { y: 2, x: 9 },
+      { y: 3, x: 0 },
+      { y: 3, x: 1 },
+      { y: 3, x: 2 },
+      { y: 3, x: 3 },
+      { y: 3, x: 4 },
+      { y: 3, x: 5 },
+      { y: 3, x: 6 },
+      { y: 3, x: 7 },
+      { y: 3, x: 8 },
+      { y: 3, x: 9 },
+      { y: 4, x: 0 },
+      { y: 4, x: 1 },
+      { y: 4, x: 2 },
+      { y: 4, x: 3 },
+      { y: 4, x: 4 },
+      { y: 4, x: 5 },
+      { y: 4, x: 6 },
+      { y: 4, x: 7 },
+      { y: 4, x: 8 },
+      { y: 4, x: 9 },
+      { y: 5, x: 0 },
+      { y: 5, x: 1 },
+      { y: 5, x: 2 },
+      { y: 5, x: 3 },
+      { y: 5, x: 4 },
+      { y: 5, x: 5 },
+      { y: 5, x: 6 },
+      { y: 5, x: 7 },
+      { y: 5, x: 8 },
+      { y: 5, x: 9 },
+      { y: 6, x: 0 },
+      { y: 6, x: 1 },
+      { y: 6, x: 2 },
+      { y: 6, x: 3 },
+      { y: 6, x: 4 },
+      { y: 6, x: 5 },
+      { y: 6, x: 6 },
+      { y: 6, x: 7 },
+      { y: 6, x: 8 },
+      { y: 6, x: 9 },
+      { y: 7, x: 0 },
+      { y: 7, x: 1 },
+      { y: 7, x: 2 },
+      { y: 7, x: 3 },
+      { y: 7, x: 4 },
+      { y: 7, x: 5 },
+      { y: 7, x: 6 },
+      { y: 7, x: 7 },
+      { y: 7, x: 8 },
+      { y: 7, x: 9 },
+      { y: 8, x: 0 },
+      { y: 8, x: 1 },
+      { y: 8, x: 2 },
+      { y: 8, x: 3 },
+      { y: 8, x: 4 },
+      { y: 8, x: 5 },
+      { y: 8, x: 6 },
+      { y: 8, x: 7 },
+      { y: 8, x: 8 },
+      { y: 8, x: 9 },
+      { y: 9, x: 0 },
+      { y: 9, x: 1 },
+      { y: 9, x: 2 },
+      { y: 9, x: 3 },
+      { y: 9, x: 4 },
+      { y: 9, x: 5 },
+      { y: 9, x: 6 },
+      { y: 9, x: 7 },
+      { y: 9, x: 8 },
+    ];
 
-//     while (shipsToPlace.length > 0) {
-//       let ship = shipsToPlace.shift();
-//       aiPlayer.placeShip(ship);
-//       shipsPlaced += 1;
-//       expect(aiPlayer.gameboard.ships.length).toBe(shipsPlaced);
-//     }
+    const firstCoordinate = aiPlayer.getCoordinates(new Gameboard());
+    expect(firstCoordinate).toMatchObject({ y: 9, x: 9 });
+    aiPlayer.attackedSquares.push({ y: 9, x: 9 });
 
-//     expect(aiPlayer.gameboard.ships.length).toBe(5);
-//   });
-// });
+    console.log(aiPlayer.attackedSquares.shift());
+    const secondCoordinate = aiPlayer.getCoordinates(new Gameboard());
+    expect(secondCoordinate).toMatchObject({ y: 0, x: 0 });
+  });
 
-// describe('Testing the Human player', () => {
-//   beforeAll(() => {
-//     humanPlayer = new Human();
-//   });
+  test('Ensure rememberAttack() base case', () => {
+    aiPlayer.rememberAttack({ y: 5, x: 3 });
+    expect(aiPlayer.attackedSquares).toHaveLength(1);
+    expect(aiPlayer.attackedSquares[0]).toMatchObject({ y: 5, x: 3 });
+  });
+});
 
-//   test('Place ship', () => {
-//     const shipsToPlace = [...humanPlayer.ships];
-//     expect(humanPlayer.gameboard.ships.length).toBe(0);
-
-//     humanPlayer.placeShip(shipsToPlace.shift(), { y: 0, x: 0 });
-//     expect(humanPlayer.gameboard.ships.length).toBe(1);
-
-//     humanPlayer.placeShip(shipsToPlace.shift(), { y: 0, x: 0 });
-//     expect(humanPlayer.gameboard.ships.length).toBe(1);
-
-//     humanPlayer.placeShip(shipsToPlace.shift(), { y: 4, x: 2 });
-//     expect(humanPlayer.gameboard.ships.length).toBe(2);
-
-//     humanPlayer.placeShip(shipsToPlace.shift(), { y: 4, x: 0 });
-//     expect(humanPlayer.gameboard.ships.length).toBe(2);
-//   });
-// });
-
-test('Empty', () => {});
+describe('Testing the Human player', () => {});
