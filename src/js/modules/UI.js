58253fc2b0dcc307f2b5a547a74524ee50228c1a
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-11-14 23:44:59 +0100

    Create UI board tables and square onClick

--- src/js/modules/UI.js
+++ src/js/modules/UI.js
@@ -3,37 +3,50 @@ export default class UserInterface {
   player2Container = document.querySelector('#player2');
   player1Tables;
   player2Tables;
+  coordinatesResolver = null;
 
   constructor() {
     this.player1Tables = this.createPlayerBoards(this.player1Container);
     this.player2Tables = this.createPlayerBoards(this.player2Container);
   }
 
+  onSquareClick(event) {
+    const coordinates = { y: event.target.dataset.y, x: event.target.dataset.x };
+    if (this.coordinatesResolver !== null) {
+      this.coordinatesResolver(coordinates);
+      this.coordinatesResolver = null;
+    }
+  }
+
   updateSquare(table, { y, x }, classNames) {
     const selector = `tr:nth-of-type(${y + 1}) td:nth-of-type(${x + 1})`;
     const square = table.querySelector(selector);
-    console.log('CLASS NAMES: ', classNames);
     square.classList.add(...classNames);
   }
 
-  createPlayerBoards(playerContainer) {
-    function createTable(className) {
-      const table = document.createElement('table');
-      table.classList.add(className);
+  createTable(className) {
+    const table = document.createElement('table');
+    table.classList.add(className);
 
-      for (let row = 0; row < 10; row += 1) {
-        const tr = document.createElement('tr');
-        for (let col = 0; col < 10; col += 1) {
-          const td = document.createElement('td');
-          tr.appendChild(td);
-        }
-        table.appendChild(tr);
+    for (let row = 0; row < 10; row += 1) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 10; col += 1) {
+        const td = document.createElement('td');
+        td.dataset.x = col;
+        td.dataset.y = row;
+
+        td.addEventListener('click', this.onSquareClick.bind(this));
+
+        tr.appendChild(td);
       }
-      return table;
+      table.appendChild(tr);
     }
+    return table;
+  }
 
-    const playerTable = createTable('player');
-    const opponentTable = createTable('opponent');
+  createPlayerBoards(playerContainer) {
+    const playerTable = this.createTable('player');
+    const opponentTable = this.createTable('opponent');
 
     playerContainer.appendChild(playerTable);
     playerContainer.appendChild(opponentTable);
