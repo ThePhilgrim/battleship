127bfeb3f3ea955cd61be1c202d13f028a75ad00
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-11-17 20:30:28 +0100

    Work on making ships draggable for placing them

--- src/js/modules/UI.js
+++ src/js/modules/UI.js
@@ -10,7 +10,57 @@ export default class UserInterface {
     this.player2Board = this.createPlayerBoard(this.player2Container);
   }
 
-  onSquareClick(event) {
+  // TODO: Ship moves strangely when zoomed in
+  // Solution: Check DPI, calculate size of 1px (real pixels), multiply calc accordingly
+  // TODO: Bug – Drag ship to opponent board -> Click rotate button
+  onDragStart(event, ship) {
+    const square = event.target;
+    const index = square.dataset.index;
+    const shipElement = square.closest('.ship');
+
+    const shipElementX = shipElement.offsetLeft;
+    const shipElementY = shipElement.offsetTop;
+    const initialMouseX = event.screenX;
+    const initialMouseY = event.screenY;
+
+    shipElement.classList.add('being-dragged');
+    shipElement.style.left = `${shipElementX}px`;
+    shipElement.style.top = `${shipElementY}px`;
+
+    function onMouseMove(event) {
+      const deltaX = event.screenX - initialMouseX;
+      const deltaY = event.screenY - initialMouseY;
+      shipElement.style.left = `${shipElementX + deltaX}px`;
+      shipElement.style.top = `${shipElementY + deltaY}px`;
+    }
+
+    function onMouseUp(event) {
+      shipElement.classList.remove('being-dragged');
+      if (!event.target.closest('table').classList.contains('player')) {
+        return;
+      }
+      ship.isVertical = shipElement.classList.contains('vertical');
+      const targetedSquareCoordinates = { y: event.target.dataset.y * 1, x: event.target.dataset.x * 1 };
+
+      let adjustedCoordinates;
+      if (ship.isVertical) {
+        adjustedCoordinates = { y: targetedSquareCoordinates.y - index, x: targetedSquareCoordinates.x };
+      } else {
+        adjustedCoordinates = { y: targetedSquareCoordinates.y, x: targetedSquareCoordinates.x - index };
+      }
+
+      removeEventListener('mousemove', onMouseMove);
+      removeEventListener('mouseup', onMouseUp);
+      ship.hasBeenPlaced(adjustedCoordinates);
+    }
+
+    addEventListener('mousemove', onMouseMove);
+    addEventListener('mouseup', onMouseUp);
+  }
+
+  // onSquarePlaceShip(event) {}
+
+  onSquareAttack(event) {
     const coordinates = { y: event.target.dataset.y, x: event.target.dataset.x };
     if (this.coordinatesResolver !== null) {
       this.coordinatesResolver(coordinates);
@@ -35,7 +85,7 @@ export default class UserInterface {
         td.dataset.x = col;
         td.dataset.y = row;
 
-        td.addEventListener('click', this.onSquareClick.bind(this));
+        td.addEventListener('mousedown', this.onSquareAttack.bind(this));
 
         tr.appendChild(td);
       }
@@ -44,23 +94,53 @@ export default class UserInterface {
     return table;
   }
 
+  createShip(ship) {
+    const shipElement = document.createElement('li');
+    shipElement.classList.add('ship', ship.shipType.toLowerCase(), 'vertical');
+    shipElement.dataset.size = ship.size;
+
+    for (let i = 0; i < ship.size; i += 1) {
+      const square = document.createElement('div');
+      square.dataset.index = i;
+
+      square.addEventListener('mousedown', (event) => {
+        this.onDragStart(event, ship);
+      });
+
+      shipElement.appendChild(square);
+    }
+
+    return shipElement;
+  }
+
   putShipsInYard(player, board) {
-    board.shipYard.innerHTML = '';
+    const ships = board.shipYard.querySelector('.ships');
+    ships.innerHTML = '';
     player.ships.forEach((ship) => {
-      const shipElement = document.createElement('li');
-      shipElement.classList.add('ship', ship.shipType.toLowerCase());
-      shipElement.dataset.size = ship.size;
-      for (let i = 1; i <= ship.size; i += 1) {
-        const square = document.createElement('div');
-        shipElement.appendChild(square);
-      }
-      board.shipYard.appendChild(shipElement);
+      const shipElement = this.createShip(ship);
+      ships.appendChild(shipElement);
     });
   }
 
   createShipYard() {
-    const shipYard = document.createElement('ul');
+    const shipYard = document.createElement('div');
     shipYard.classList.add('shipyard');
+
+    const ships = document.createElement('ul');
+    ships.classList.add('ships');
+    shipYard.appendChild(ships);
+
+    const rotateButton = document.createElement('button');
+    rotateButton.textContent = 'Rotate';
+
+    rotateButton.addEventListener('click', () => {
+      const shipElements = shipYard.querySelectorAll('.ship');
+      shipElements.forEach((ship) => {
+        ship.classList.toggle('vertical');
+        ship.classList.toggle('horizontal');
+      });
+    });
+    shipYard.appendChild(rotateButton);
     return shipYard;
   }
 
